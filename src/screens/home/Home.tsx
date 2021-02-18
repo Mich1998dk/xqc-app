558e90595b006f55bf1c39bfeba2d844573a693e
Thomas Holstein Qvortrup <thqv@itu.dk>
2021-02-18 15:52:58 +0100

    Demo of reponsive rendering

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, FlatList, View, TouchableOpacity } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { HomeStackParamList } from "../../utils/types";
 import { Text } from "../../components/atoms/index";
@@ -15,6 +15,7 @@ type Props = {
 
 export default function Home({ navigation }: Props) {
   const [state, setState] = useState({ loading: false });
+  const [selected, setSelected] = useState<number[]>([]);
   const dispatch = useDispatch();
   const redux = useSelector((state) => state);
 
@@ -24,13 +25,50 @@ export default function Home({ navigation }: Props) {
     return unsubscribe;
   }, [navigation]);
 
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
   return (
     <Container>
       <>
         <Header title="XQC" />
+        <FlatList
+          columnWrapperStyle={{ justifyContent: "space-between" }}
+          data={data}
+          numColumns={2}
+          style={{ paddingHorizontal: "4%" }}
+          renderItem={({ item, index }) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  if (selected.includes(item)) {
+                    const index = selected.indexOf(item);
+                    var temp = [...selected];
+                    temp.splice(index, 1);
+                    setSelected(temp);
+                  } else {
+                    var newArray = [...selected, item];
+                    setSelected(newArray);
+                  }
+                }}
+                key={item}
+                style={[
+                  styles.box,
+                  selected.includes(item) ? { backgroundColor: "#484848" } : {},
+                ]}
+              ></TouchableOpacity>
+            );
+          }}
+        />
       </>
     </Container>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  box: {
+    width: "48%",
+    backgroundColor: "#888",
+    height: 180,
+    marginTop: 10,
+  },
+});
