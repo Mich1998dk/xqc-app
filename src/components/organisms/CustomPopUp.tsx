1d57bec04c846df28d7a3489fa7513460df15eb6
mmau <mmau@itu.dk>
2022-04-18 12:22:25 +0200

    documentation and cleanup of CustomPopUp

--- src/components/organisms/CustomPopUp.tsx
+++ src/components/organisms/CustomPopUp.tsx
@@ -1,9 +1,7 @@
-import React, { CSSProperties, SyntheticEvent, useState } from 'react';
-import { StyleSheet, View, Text, Modal, Pressable } from 'react-native';
-import { Popup } from 'reactjs-popup'
+import React, { CSSProperties } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Popup } from 'reactjs-popup';
 import { colors, fonts } from '../../utils/theme';
-import { Model } from '../../utils/types';
-import { Button } from '../molecules';
 interface Props {
     title: string;
     contentText: string;
@@ -12,9 +10,22 @@ interface Props {
     buttons: any[];
 }
 
+/**
+ * A custom popup that will have the same styling as rest of XQC.
+ * This is a generic callable class which means it is reusable everywhere a popup is needed
+ *
+ * @param title The title of the popup, this is a @type {string}
+ * @param contentText The main text of the popup, this is a @type {string}
+ * @param visible The visibility of the popup, this is a @type {boolean} with true meaning visible and false meaning invisible
+ * @param onClose A @type{Function} to determine what happens when you close the popup
+ * @param buttons An array of buttons, these are the buttons showed at the bottom of the popup, this is a @type {any[]}
+ *
+ * @returns The custom popup as a HTML style code
+ */
 export default function CustomPopUp({ title, contentText, visible, onClose, buttons}: Props) {
-    
-    const contentTextLines = contentText.split('\n').map(line => <p key={line}>{line}</p>)
+
+    //A custom way to implement the use of '\n' in the contentText as newlines
+    const contentTextLines = contentText.split("\n").map(line => <p key={line}>{line}</p>);
 
     return (
         <Popup
@@ -35,13 +46,14 @@ export default function CustomPopUp({ title, contentText, visible, onClose, butt
 
 
 }
+//The styles used
 const styleSheet = {
-
+    //The style used for the main content
     contentStyle: {
         height: "auto",
         width: "auto",
-        overflow: 'auto',
-        overflowX: 'hidden',
+        overflow: "auto",
+        overflowX: "hidden",
         padding: "0 1% 1% 1%",
         backgroundColor: colors.background,
         borderStyle: "solid",
@@ -50,15 +62,16 @@ const styleSheet = {
         borderRadius: "25px",
         fontFamily: fonts.med,
         color: colors.white,
-        textAlign: 'center',
+        textAlign: "center",
     } as CSSProperties,
 
-
+    //The style used for everything around the popup (making the sides darker but transparent)
     overlayStyle: {
         backgroundColor: colors.popupBackground
     } as CSSProperties,
 }
 
+//The style used for buttons to be in rows with spacing between
 const styles = StyleSheet.create({
     buttonsView: {
         flexDirection: "row",
